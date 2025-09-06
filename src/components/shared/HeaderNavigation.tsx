b85cbf9c22b73bb7b24167a6326fdfdbeea303d4
Marko Penava <penavamarko482@gmail.com>
2025-09-06 18:28:52 +0200

    feat: improve header and change global colors

--- src/components/shared/HeaderNavigation.tsx
+++ src/components/shared/HeaderNavigation.tsx
@@ -3,15 +3,8 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "../ui/button";
 import { Calendar, MenuIcon } from "lucide-react";
 
@@ -23,31 +16,22 @@ const links = {
   certificates: "#certificates",
 };
 
+const menuLinkStyle =
+  "relative h-9 px-4 py-2 before:absolute before:bottom-0 before:left-1/4 before:h-0.5 before:w-1/2 before:scale-x-0 before:bg-primary before:transition-transform hover:before:scale-x-100";
+
 const HeaderNavigation = () => {
   return (
-    <header className="sticky top-0 z-50 bg-white/75 shadow-md backdrop-blur-md">
+    <header className="sticky top-0 z-50 border-b backdrop-blur-md">
       <nav className="container mx-auto flex max-w-6xl items-center justify-between p-4">
-        <a
-          href={links.home}
-          className="flex items-center gap-2 text-xl font-bold"
-        >
-          PORTFOLIO
-        </a>
         <NavigationMenu className="hidden lg:block">
           <NavigationMenuList>
             <NavigationMenuItem>
-              <NavigationMenuLink
-                href={links.home}
-                className={navigationMenuTriggerStyle()}
-              >
+              <NavigationMenuLink href={links.home} className={menuLinkStyle}>
                 Home
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink
-                href={links.about}
-                className={navigationMenuTriggerStyle()}
-              >
+              <NavigationMenuLink href={links.about} className={menuLinkStyle}>
                 About
               </NavigationMenuLink>
             </NavigationMenuItem>
@@ -55,35 +39,31 @@ const HeaderNavigation = () => {
             <NavigationMenuItem>
               <NavigationMenuLink
                 href={links.education}
-                className={navigationMenuTriggerStyle()}
+                className={menuLinkStyle}
               >
                 Education & Career
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink
-                href={links.skills}
-                className={navigationMenuTriggerStyle()}
-              >
+              <NavigationMenuLink href={links.skills} className={menuLinkStyle}>
                 Skills
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
               <NavigationMenuLink
                 href={links.certificates}
-                className={navigationMenuTriggerStyle()}
+                className={menuLinkStyle}
               >
                 Certificates
               </NavigationMenuLink>
             </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Button>
-                <Calendar />
-                Book a Meeting
-              </Button>
-            </NavigationMenuItem>
+            <NavigationMenuItem></NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
+        <Button>
+          <Calendar />
+          Book a Meeting
+        </Button>
         <Sheet>
           <SheetTrigger asChild className="lg:hidden">
             <Button variant="outline" size="icon" aria-label="Menu">
@@ -91,13 +71,6 @@ const HeaderNavigation = () => {
             </Button>
           </SheetTrigger>
           <SheetContent side="top" className="max-h-dvh overflow-auto">
-            <SheetHeader>
-              <SheetTitle>
-                <a href={links.home} className="flex items-center gap-2 pb-8">
-                  Portfolio
-                </a>
-              </SheetTitle>
-            </SheetHeader>
             <div className="flex flex-col gap-6">
               <a href={links.home} className="font-medium">
                 Home
