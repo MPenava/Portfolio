1c02b59e3f01ebe9709bb63eb8e2617f20e3b33c
Marko Penava <penavamarko482@gmail.com>
2025-10-25 15:37:17 +0200

    feat: profile image and changed theme colors

--- src/components/shared/ProfileImage.tsx
+++ src/components/shared/ProfileImage.tsx
@@ -1,13 +1,23 @@
+import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import mpen from "@/assets/profile/mpen.jpg";
 
 const ProfileImage = () => {
   return (
-    <Avatar className="size-56 border-8">
-      <AvatarImage src={mpen.src} alt="Profile image" />
-      <AvatarFallback>Profile image</AvatarFallback>
-    </Avatar>
+    <div className="relative inline-block">
+      <Avatar className="size-72 rounded-3xl border-4">
+        <AvatarImage src={mpen.src} alt="Profile image" className="shadow-xl" />
+        <AvatarFallback>Profile image</AvatarFallback>
+      </Avatar>
+      {/* Badges */}
+      <div className="absolute -left-8 top-24 rounded-md bg-background px-3 py-2 shadow-md shadow-primary-foreground">
+        Code
+      </div>
+      <span className="absolute -right-10 top-16 rounded-md bg-background px-3 py-2 shadow-md shadow-primary-foreground">
+        Design
+      </span>
+    </div>
   );
 };
 
