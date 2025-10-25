79c76b19f3c905bd99540a5a5e28ae546f90353a
Marko Penava <penavamarko482@gmail.com>
2025-10-25 21:19:30 +0200

    feat: button customizing

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-accent-foreground text-foreground bg-accent shadow-sm hover:bg-accent/90 hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -22,7 +22,7 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-lg px-3 text-xs",
-        lg: "h-10 rounded-lg px-8",
+        lg: "h-11 rounded-lg px-8",
         icon: "h-9 w-9",
       },
     },
