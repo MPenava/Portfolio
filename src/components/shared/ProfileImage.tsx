f931abea378c8ed23821a6c5482627014e7adb54
MPenava <marko.penava@fpmoz.sum.ba>
2025-10-26 18:44:56 +0100

    feat: change profile image format

--- src/components/shared/ProfileImage.tsx
+++ src/components/shared/ProfileImage.tsx
@@ -1,10 +1,10 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-import mpen from "@/assets/images/mpen.jpg";
+import mpen from "@/assets/images/mpen.png";
 
 const ProfileImage = () => {
   return (
-    <Avatar className="h-[16rem] w-[16rem] rounded-full">
+    <Avatar className="size-60 rounded-full">
       <AvatarImage src={mpen.src} alt="Profile image" />
       <AvatarFallback>Profile image</AvatarFallback>
     </Avatar>
